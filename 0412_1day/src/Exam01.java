f7dfae9afef4fc47e9ddb8b75134c413a4d4ee9e
SM2122 <SM2122@DESKTOP-IIR1TFJ>
2021-06-23 10:36:53 +0900

    위대한 첫 번째 걸음

---
+++ 0412_1day/src/Exam01.java
@@ -0,0 +1,22 @@
+public class Exam01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 변수 선언
+		int a = 5;
+		System.out.print(a);
+		
+		// console 창으로 변수 "출력"
+		System.out.print(a);
+		
+		// 상수 선언
+		final int b = 3;
+		
+		// a = 3; (변경가능, 상수가 아니기 때문에 변경 가능)
+		// b = 5; (변경불가능, 상수는 불변하는 수이기 때문에)
+		System.out.println(b);
+		
+	}
+
+}
+
