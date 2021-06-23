512a5f343eea9042c208d49bc9b450fc91c26ede
SM2122 <SM2122@DESKTOP-IIR1TFJ>
2021-06-23 10:40:09 +0900

    Exam02

---
+++ 0412_1day/src/Exam02.java
@@ -0,0 +1,19 @@
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// java에서 똑같은 이름의 변수를 2개 이상 만드는 것은 불가능하다.
+		int a = 8;
+		
+		// int a = 9; (앞에서 int a = 8; 존재하기 때문에 만들 수 없음.)
+		// a = 9; 는 가능 (기존 변수값 변경)
+		// 숫자로 시작하는 변수는 만들 수 없음
+		// 항상 변수의 시작은 소문자로 한다.
+		// camel 기법 사용 ex) int myNameIsLee = 8;
+		
+		
+	}
+
+}
