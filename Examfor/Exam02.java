f54006343a98679ee6057b32d2bdf96a85f3157f
noory-kwon <70535179+noory-kwon@users.noreply.github.com>
2021-06-23 12:45:15 +0900

    0414_3day

---
+++ Examfor/Exam02.java
@@ -0,0 +1,16 @@
+package Examfor;
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		
+		for (int i = 1; i <= 100; i++) {	
+			sum += i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
---
+++ Examfor/Exam04.java
@@ -0,0 +1,13 @@
+package Examfor;
+
+public class Exam04 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 9; i++) {
+			System.out.println("2*" + i + "=" + 2*i);
+		}
+
+	}
+
+}
